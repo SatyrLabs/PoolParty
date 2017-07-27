0ede503aa302a71fc0b26d26505774b02edc29ea
Michael Highsmith <mhighsm1@slu.edu>
2017-07-27 15:07:54 -0500

    pulling 4 max

--- app/src/main/java/com/example/sisyphus/thepoolparty/MainActivity.java
+++ app/src/main/java/com/example/sisyphus/thepoolparty/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.sisyphus.thepoolparty;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Initialize the authentication object and state listener
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //Check if user is logged in or not.  If not, show login screen
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    //user is signed in
+
+                } else{
+                    //user is signed out
+                    
+                }
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
